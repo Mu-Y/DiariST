8916fbc3314391f01575b83ab56df20fff6d8a52
Naoyuki Kanda <Naoyuki.Kanda@microsoft.com>
2023-09-13 12:24:09 -0400

    simplify download script

--- diarist/data/download_alimeeting_translation.sh
+++ diarist/data/download_alimeeting_translation.sh
@@ -3,11 +3,8 @@
 outdir=$1
 
 mkdir -p  $outdir/AliMeetingTranslation
-cd $outdir/AliMeetingTranslation
 
-curl https://www.microsoft.com/en-us/research/uploads/prod/2023/09/README.txt -o README.txt
-curl https://www.microsoft.com/en-us/research/uploads/prod/2023/09/test.txt -o test.txt
-curl https://www.microsoft.com/en-us/research/uploads/prod/2023/09/dev.txt -o dev.txt
-curl https://www.microsoft.com/en-us/research/uploads/prod/2023/09/train.txt -o train.txt
-
-cd -
+curl https://www.microsoft.com/en-us/research/uploads/prod/2023/09/readme.txt -o $outdir/AliMeetingTranslation/readme.txt
+curl https://www.microsoft.com/en-us/research/uploads/prod/2023/09/test.txt -o $outdir/AliMeetingTranslation/test.txt
+curl https://www.microsoft.com/en-us/research/uploads/prod/2023/09/dev.txt -o $outdir/AliMeetingTranslation/dev.txt
+curl https://www.microsoft.com/en-us/research/uploads/prod/2023/09/train.txt -o $outdir/AliMeetingTranslation/train.txt
